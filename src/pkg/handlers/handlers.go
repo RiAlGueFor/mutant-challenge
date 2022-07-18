f76263f79f357396596e274b06d18ea9e17fbf56
RiAlGueFor <rialguefor1985@gmail.com>
2022-07-18 09:31:06 -0500

    Moviendo variables

--- src/pkg/handlers/handlers.go
+++ src/pkg/handlers/handlers.go
@@ -19,19 +19,10 @@ type ErrorBody struct{
   ErrorMsg *string `json:"error, omitempty"`
 }
 
-type DNAChain struct {
-  DNA []string `json:"dna, omitempty"`
-}
-
 type Stats struct{
   MutantDNA int `json:"count_mutant_dna, omitempty"`
   HumanDNA int `json:"count_human_dna, omitempty"`
   Ratio float32 `json:"ratio, omitempty"`
-}
-
-type DNARecord struct {
-  DNA string `json:"dna, omitempty"`
-  IsMutant bool `json:"isMutant, omitempty"`
 }
 
 func GetStats(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(
@@ -64,54 +55,17 @@ func GetStats(req events.APIGatewayProxyRequest, tableName string, dynaClient dy
 }
 
 func CheckMutantDNA(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(*events.APIGatewayProxyResponse, error){
-  var dnaChain DNAChain
-  if err := json.Unmarshal([]byte(req.Body), &dnaChain); err!=nil {
-    return apiResponse(http.StatusForbidden, ErrorBody{
-      aws.String(ErrorFailedToUnmarshalRecord),
-    })
-	}
-  // 1 - Check if DNA is valid
-  var isValid=validators.IsDNAValid(dnaChain.DNA)
-  if !isValid {
-    return apiResponse(http.StatusForbidden, ErrorBody{
-      aws.String(ErrorInvalidDNAChain),
-    })
-  }
-  // 2 - Check if DNA was previously validated. If it was, return the isMutant flag
-  var dnaRecord DNARecord
-  dnaJoin := strings.Join(dnaChain.DNA, "-")
-  dnaJoin = strings.Replace(dnaJoin,"-","\",\"",-1)
-  dnaRecord.DNA = "[\""+ dnaJoin +"\"]"
-  currentDNA, _:=dynamoDB_API.FetchDNARecord(dnaRecord.DNA,tableName,dynaClient)
-  if currentDNA!=nil && len(currentDNA.DNA)>0 {
-    if currentDNA.IsMutant{
-      return apiResponse(http.StatusOK, nil)
+  result, err := mutantDNA.InitScanning(req, tableName, dynaClient)
+	if err!=nil {
+    if err!="" {
+      return apiResponse(http.StatusBadRequest, ErrorBody{
+  			aws.String(err.Error()),
+  		})
     } else {
-      return apiResponse(http.StatusForbidden, nil)
+      apiResponse(http.StatusBadRequest, nil)
     }
-  }
-  // 3 - If it wasn't validated, go on with the validation
-  dnaRecord.IsMutant = true
-  chainLetters := [3]string{ "A", "C", "G" }
-  for k := 0 ; k < len(chainLetters); k++ {
-    if (!ScanningDNA(dnaChain.DNA,chainLetters[k])) {
-      dnaRecord.IsMutant=false
-      break;
-    }
-  }
-  // 4 - After Checking the DNA, Store DNA Chain and Validation Result on DynamoDB
-  _, err:= dynamoDB_API.CreateRecordDNA(dnaRecord,tableName,dynaClient)
-  if err!=nil{
-    return apiResponse(http.StatusBadRequest, ErrorBody{
-      aws.String(err.Error()),
-    })
-  }
-
-  if dnaRecord.IsMutant {
-    return apiResponse(http.StatusOK, nil)
-  } else {
-    return apiResponse(http.StatusForbidden, nil)
-  }
+	}
+	return apiResponse(http.StatusOK, nil)
 }
 
 func UnhandledMethod()(*events.APIGatewayProxyResponse, error){
--- src/pkg/mutantDNA/mutantDNA.go
+++ src/pkg/mutantDNA/mutantDNA.go
@@ -3,7 +3,71 @@ package mutantDNA
 import(
   "encoding/json"
   "errors"
+  "github.com/RiAlGueFor/mutant-challenge/src/pkg/dynamoDB"
+	"net/http"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
+
+var (
+  ErrorFailedToUnmarshalRecord = "Failed to Unmarshall Record"
+  ErrorInvalidDNAChain = "Invalid DNA Chain"
+)
+
+type DNAChain struct {
+  DNA []string `json:"dna, omitempty"`
+}
+
+type DNARecord struct {
+  DNA string `json:"dna, omitempty"`
+  IsMutant bool `json:"isMutant, omitempty"`
+}
+
+func InitScanning(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(*DNARecord, error){
+  var dnaChain DNAChain
+  if err := json.Unmarshal([]byte(req.Body), &dnaChain); err!=nil {
+    return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+  // 1 - Check if DNA is valid
+  var isValid=validators.IsDNAValid(dnaChain.DNA)
+  if !isValid {
+    return nil, errors.New(ErrorInvalidDNAChain)
+  }
+  // 2 - Check if DNA was previously validated. If it was, return the isMutant flag
+  var dnaRecord DNARecord
+  dnaJoin := strings.Join(dnaChain.DNA, "-")
+  dnaJoin = strings.Replace(dnaJoin,"-","\",\"",-1)
+  dnaRecord.DNA = "[\""+ dnaJoin +"\"]"
+  currentDNA, _:=dynamoDB_API.FetchDNARecord(dnaRecord.DNA,tableName,dynaClient)
+  if currentDNA!=nil && len(currentDNA.DNA)>0 {
+    if currentDNA.IsMutant{
+      return &dnaRecord, nil
+    } else {
+      return nil, errors.New("")
+    }
+  }
+  // 3 - If it wasn't validated, go on with the validation
+  dnaRecord.IsMutant = true
+  chainLetters := [3]string{ "A", "C", "G" }
+  for k := 0 ; k < len(chainLetters); k++ {
+    if (!ScanningDNA(dnaChain.DNA,chainLetters[k])) {
+      dnaRecord.IsMutant=false
+      break;
+    }
+  }
+  // 4 - After Checking the DNA, Store DNA Chain and Validation Result on DynamoDB
+  _, err:= dynamoDB_API.CreateRecordDNA(dnaRecord,tableName,dynaClient)
+  if err!=nil{
+    return nil, err
+  }
+
+  if dnaRecord.IsMutant {
+    return &dnaRecord, nil
+  } else {
+    return nil, errors.New("")
+  }
+}
 
 func ScanningDNA(dna []string, letter2Check string) bool {
   oCounter:=0
